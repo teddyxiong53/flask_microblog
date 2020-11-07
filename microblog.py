fc8a908395cc8e09da1de5de34a6234cb1fb2f02
teddyxiong53 <1073167306@qq.com>
2020-11-07 16:19:16 +0800

    增加数据库支持

--- microblog.py
+++ microblog.py
@@ -1,1 +1,13 @@
 from app import app
+from app.models import  User, Post
+from app import db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
+
+
