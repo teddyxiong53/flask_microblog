565bd602fbb39edbccf8ea3ecbd06371d1f02d37
teddyxiong53 <1073167306@qq.com>
2020-11-07 17:18:08 +0800

    增加用户注册功能

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField(label='Username', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
     remember_me = BooleanField(label='Remember me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField(label='Username', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    password2 = PasswordField(label='Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('用户名已经被占用了')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('邮箱已经被占用了')
 
