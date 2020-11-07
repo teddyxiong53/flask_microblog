088f9c23361153730b75a6dbb9b4beffdd2b50b4
teddyxiong53 <1073167306@qq.com>
2020-11-07 14:53:43 +0800

    增加登陆界面

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
 import flask_wtf
+from flask import flash, redirect, url_for
+from app.forms import  LoginForm
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -22,4 +25,15 @@ def index():
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('login request for user {}, rember me = {}'.format(
+            form.username.data,
+            form.remember_me.data
+        ))
+        return redirect(url_for('index')) # 不能写成/index。
+    return render_template('login.html', title='Sign In', form=form)
 
